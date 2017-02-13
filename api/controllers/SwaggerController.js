85c5054ff124986314cf186a2925e6b03c39c138
Evgeni Atanasov <evgeni.atanasov@Softwaregroup-bg.com>
2017-02-13 16:23:51 +0200

    Configure Swagger for Api documentation.

---
+++ api/controllers/SwaggerController.js
@@ -0,0 +1,9 @@
+// api/controllers/SwaggerController.js
+
+var _ = require( 'lodash' );
+var _super = require( 'sails-swagger/dist/api/controllers/SwaggerController' );
+
+_.merge( exports, _super );
+_.merge( exports, {
+  // Extend with custom logic here by adding additional fields, methods, etc.
+} );
---
+++ config/installedHooks.js
@@ -0,0 +1,7 @@
+// config/installedHooks.js
+module.exports.installedHooks = {
+    "sails-swagger": {
+    // load the hook into sails.hooks.swagger instead of sails.hooks['sails-swagger']
+        "name": "swagger"
+    }
+};
